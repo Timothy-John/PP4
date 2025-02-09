b3c504e653188ab80f58f45d4633c977d3baf988
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-02-09 15:43:09 +0530

    Bug Fixes: cqt_loader.py

--- cqt_loader.py
+++ cqt_loader.py
@@ -28,7 +28,7 @@ class IndianCover(Dataset):
     def __getitem__(self, index):
         filename = self.file_list[index].strip()
         set_id = filename.split('_')[0]  # Assuming the set_id is the first part before '_'
-        in_path = self.indir +filename.split('_')[:-1] +'/' +filename +'.mp3'
+        in_path = self.indir +filename[:-int(len(filename.split('_')[-1])+1)] +'/' +filename +'.mp3'
         data, sr = librosa.load(in_path, sr=24000)
         return (data, sr), int(set_id)
 

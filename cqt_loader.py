3a2dd88441099c46327718aadc78f8c0278e35ea
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-02-17 09:32:17 +0530

    Update cqt_loader.py

--- cqt_loader.py
+++ cqt_loader.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 
 class IndianCover(Dataset):
     def __init__(self, mode='train', out_length=None):
-        self.indir = '/content/drive/MyDrive/CoverSongDetection/CoverIndian_audio_corrected/'
+        self.indir = '/content/drive/MyDrive/CoverSongDetection_Timothy/CoverIndian_audio/'
         if mode=='train':
           self.filepath = 'data/coversIndian_train_val.txt'
         elif mode=='val':

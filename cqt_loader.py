0cf6c8d04606cc69f889f4615d3b99e6d23ae1bb
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-02-22 11:08:58 +0530

    Update cqt_loader.py

--- cqt_loader.py
+++ cqt_loader.py
@@ -10,7 +10,8 @@ import torch.nn.functional as F
 
 class IndianCover(Dataset):
     def __init__(self, mode='train', model='CQTNet', out_length=None):
-        if model=='MERT':
+        self.model = model
+        if self.model=='MERT':
             self.indir = '/content/drive/MyDrive/CoverSongDetection_Timothy/CoverIndian_audio/'
         else:
             self.indir = '/content/drive/MyDrive/CoverSongDetection_Timothy/IndianCover_cqt_npy'
@@ -32,6 +33,18 @@ class IndianCover(Dataset):
     def __getitem__(self, index):
         filename = self.file_list[index].strip()
         set_id = filename.split('_')[0]  # Assuming the set_id is the first part before '_'
-        in_path = self.indir +filename[:-int(len(filename.split('_')[-1])+1)] +'/' +filename +'.mp3'
-        data, sr = librosa.load(in_path, sr=24000)
+        if self.model=='CQTNet':
+            in_path = os.path.join(self.indir, filename + '.npy')
+            data = np.load(in_path)
+            transform_test = transforms.Compose([
+                lambda x: x.astype(np.float32) / (np.max(np.abs(x)) + 1e-6),
+                lambda x: self.cut_data_front(x, self.out_length),
+                lambda x: torch.Tensor(x),
+                lambda x: x.unsqueeze(0),  # Add channel dimension
+            ])
+            data = transform_test(data)
+            data = self.pad_or_truncate(data, 768, 84) #768 is the embedding size of MERT
+        else:
+            in_path = self.indir +filename[:-int(len(filename.split('_')[-1])+1)] +'/' +filename +'.mp3'
+            data, sr = librosa.load(in_path, sr=24000)
         return data, int(set_id)

da294e656c0d2737c9abd71780637573612aa147
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-06 14:12:24 +0530

    Update CQTNet.py

--- models/CQTNet.py
+++ models/CQTNet.py
@@ -9,14 +9,12 @@ from .basic_module import BasicModule
 class CQTNet(BasicModule):
     def __init__(self):
         super().__init__()
-        self.fc0 = nn.Linear(768, 500)
-        self.fc1 = nn.Linear(500, 300)
-        self.fc2 = nn.Linear(300, 300)
+        self.fc0 = nn.Linear(768, 300)
+        self.fc1 = nn.Linear(300, 300)
 
     def forward(self, x):
         N = x.size()[0]
         x = x.view(N, -1)
-        x = self.fc0(x)
-        feature = self.fc1(x)
-        x = self.fc2(feature)
+        feature = self.fc0(x)
+        x = self.fc1(feature)
         return x, feature

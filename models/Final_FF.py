8f9a6caaef7ffd967f9eac48db2c03756c20f6e2
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-10 23:41:29 +0530

    Update Final_FF.py

--- models/Final_FF.py
+++ models/Final_FF.py
@@ -9,11 +9,11 @@ from .basic_module import BasicModule
 class Final_FF(BasicModule):
     def __init__(self):
         super().__init__()
-        self.fc0 = nn.Linear(612, 300)
+        self.fc0 = nn.Linear(400, 300)
         self.fc1 = nn.Linear(300, 300)
 
     def forward(self, x,MERT_FF_out):
-        x = torch.cat([x,MERT_FF_out])
+        x = torch.cat([x.squeeze(),MERT_FF_out.squeeze()]).unsqueeze(0)
         feat = self.fc0(x)
         x = self.fc1(feat)
         return x,feat

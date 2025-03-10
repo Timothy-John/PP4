d168ab66af7d6542dd58bef74a7ea6e883868f25
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-10 22:20:32 +0530

    Update CQTNet.py

--- models/CQTNet.py
+++ models/CQTNet.py
@@ -40,15 +40,16 @@ class CQTNet(BasicModule):
             ('norm9', nn.BatchNorm2d(512)), ('relu9', nn.ReLU(inplace=True)),
         ]))
         self.pool = nn.AdaptiveMaxPool2d((1, 1))
-        self.fc0 = nn.Linear(512, 300)
+        self.fc0 = nn.Linear(612, 300)
         self.fc1 = nn.Linear(300, 10000)
 
-    def forward(self, x):
+    def forward(self, x, MERT_FF_out):
         # input [N, C, H, W] (W = 396)
         N = x.size()[0]
         x = self.features(x)  # [N, 512, 57, 2~15]
         x = self.pool(x)
         x = x.view(N, -1)
+        x = torch.cat([x,MERT_FF_out])
         feature = self.fc0(x)
         x = self.fc1(feature)
         return x, feature

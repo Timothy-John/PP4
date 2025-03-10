9750a5640814921abe70f0900a6c488413d1dd61
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-10 22:00:53 +0530

    Create MERT_FF.py

---
+++ models/MERT_FF.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+import torchvision
+import torch.nn.functional as F
+from collections import OrderedDict
+import math
+from .basic_module import BasicModule
+
+class MERT_FF(BasicModule):
+    def __init__(self):
+        super().__init__()
+        self.fc0 = nn.Linear(1024, 600)
+        self.fc1 = nn.Linear(600, 100)
+
+    def forward(self, x):
+        x = self.fc0(x)
+        x = self.fc1(x)
+        return x

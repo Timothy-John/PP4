4fb0e977096c777603c0cd1be2466008bba5e200
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-10 23:09:23 +0530

    Create Final_FF.py

---
+++ models/Final_FF.py
@@ -0,0 +1,19 @@
+import torch
+from torch import nn
+import torchvision
+import torch.nn.functional as F
+from collections import OrderedDict
+import math
+from .basic_module import BasicModule
+
+class MERT_FF(BasicModule):
+    def __init__(self):
+        super().__init__()
+        self.fc0 = nn.Linear(612, 300)
+        self.fc1 = nn.Linear(300, 300)
+
+    def forward(self, x,MERT_FF_out):
+        x = torch.cat([x,MERT_FF_out])
+        feat = self.fc0(x)
+        x = self.fc1(feat)
+        return x,feat

99eca583fba8ab410c603cccd5ffd201fea5e850
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-14 08:45:14 +0530

    Update cqt_loader.py

--- cqt_loader.py
+++ cqt_loader.py
@@ -10,13 +10,15 @@ import torch.nn.functional as F
 
 class IndianCover(Dataset):
     def __init__(self, mode='train', out_length=None):
-        self.indir = '../CoverSongDetection_Timothy/CoverIndian_audio/'
         if mode=='train':
           self.filepath = 'data/coversIndian_train_val.txt'
+          self.indir = '../CoverSongDetection_Timothy/MERT_train_embeddings.npy'
         elif mode=='val':
           self.filepath = 'data/coversIndian_train_val.txt'
+          self.indir = '../CoverSongDetection_Timothy/MERT_train_embeddings.npy'
         else:
           self.filepath = 'data/coversIndian_test.txt'
+          self.indir = '../CoverSongDetection_Timothy/MERT_test_embeddings.npy'
         
         with open(self.filepath, 'r') as fp:
             self.file_list = [line.rstrip() for line in fp]
@@ -28,10 +30,6 @@ class IndianCover(Dataset):
     def __getitem__(self, index):
         filename = self.file_list[index].strip()
         set_id = filename.split('_')[0]  # Assuming the set_id is the first part before '_'
-        in_path = self.indir +filename[:-int(len(filename.split('_')[-1])+1)] +'/' +filename +'.mp3'
-        data, sr = librosa.load(in_path, sr=24000)
+        data = np.load(self.indir)[index]
         return data, int(set_id)
-
-if __name__ == '__main__':
-    train_dataset = CQT('train', 394)
-    trainloader = torch.utils.data.DataLoader(train_dataset, batch_size=128, num_workers=12, shuffle=True)
+        

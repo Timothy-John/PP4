a9c4d6506035d4f4f93c6a864a64975cff91bf8a
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-27 15:23:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,11 @@ import random
 from transformers import Wav2Vec2FeatureExtractor
 from transformers import AutoModel
 import librosa
+import random
 
+torch.manual_seed(0)
+np.random.seed(0)
+random.seed(0)
 
 def custom_collate(batch):
     data = [item[0][0] for item in batch]

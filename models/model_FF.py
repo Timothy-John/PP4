21d93b9ad090eaeee849b114f5f9f31930ac44d1
Timothy Alex John <62374873+Timothy-John@users.noreply.github.com>
2025-03-27 14:44:47 +0530

    Update and rename MERT_FF.py to model_FF.py

--- models/model_FF.py
+++ models/model_FF.py
@@ -6,7 +6,7 @@ from collections import OrderedDict
 import math
 from .basic_module import BasicModule
 
-class MERT_FF(BasicModule):
+class model_FF(BasicModule):
     def __init__(self):
         super().__init__()
         self.fc0 = nn.Linear(1024, 600)
